e2635baf3e75c3e35a77f6df2b1b1df888368e08
Dale Moore <dale@dmdigital.net.au>
2018-04-28 09:35:41 +1000

    minimal test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,36 +1,5 @@
-import { ILib, Lib } from "../";
-import { ResultData } from "./data/result.data";
-
-const testTimeout = 1000;
-
-beforeAll(done => {
-  done();
-});
-
-afterAll(() => {
-  return;
-});
-
-describe("Lib", () => {
-
-  beforeAll(done => {
-    done();
-  });
-
-  afterAll(() => {
-    return;
-  });
-
-  beforeEach(() => {
-    return;
-  });
-
-  afterEach(done => {
-    done();
-  });
-
-  it("should be instantiable", () => {
-    const lib = Lib;
-    return expect(new lib()).toBeInstanceOf(Lib);
+describe("Empty Test", () => {
+  it("should do something", () => {
+    return expect(false).toEqual(false);
   });
 });
